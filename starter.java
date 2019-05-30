79b4dec22497905db28408d5391fdf3affd48626
Sarah Levering <sarahlev88@gmail.com>
2019-05-30 08:27:43 -0700

    Create starter.java

---
+++ starter.java
@@ -0,0 +1,11 @@
+import pkg.*;
+import java.util.ArrayList;
+public class starter 
+{
+		
+    public static void main(String args[])
+    {
+		Picture beach = new Picture("Beach Background.jpg");
+	}
+
+}
